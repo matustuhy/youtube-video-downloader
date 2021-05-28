782e18d9fe6dbf08ec5e12180feef720dd6513d4
Matus Tuhy <matustuhy@gmail.com>
2021-05-28 20:00:52 +0200

    Disable button and input while download is pending

--- public/script.js
+++ public/script.js
@@ -1,10 +1,53 @@
-var convertBtn = document.querySelector('.convert-button');
-var URLinput = document.querySelector('.URL-input');
-convertBtn.addEventListener('click', () => {
-    console.log(`URL: ${URLinput.value}`);
-    sendURL(URLinput.value);
-});
+const convertBtn = document.getElementById('convert-button');
+const UrlInput = document.getElementById('url-input');
 
-function sendURL(URL) {
-    window.location.href = `./download?url=${URL}`;
+const fileDownload = (data, filename, mime, bom) => {
+    const blobData = (typeof bom !== 'undefined') ? [bom, data] : [data]
+    const blob = new Blob(blobData, {type: mime || 'application/octet-stream'});
+    if (typeof window.navigator.msSaveBlob !== 'undefined') {
+        // IE workaround for "HTML7007: One or more blob URLs were
+        // revoked by closing the blob for which they were created.
+        // These URLs will no longer resolve as the data backing
+        // the URL has been freed."
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else {
+        const blobURL = (window.URL && window.URL.createObjectURL) ? window.URL.createObjectURL(blob) : window.webkitURL.createObjectURL(blob);
+        const tempLink = document.createElement('a');
+        tempLink.style.display = 'none';
+        tempLink.href = blobURL;
+        tempLink.setAttribute('download', filename);
+
+        // Safari thinks _blank anchor are pop ups. We only want to set _blank
+        // target if the browser does not support the HTML5 download attribute.
+        // This allows you to download files in desktop safari if pop up blocking
+        // is enabled.
+        if (typeof tempLink.download === 'undefined') {
+            tempLink.setAttribute('target', '_blank');
+        }
+
+        document.body.appendChild(tempLink);
+        tempLink.click();
+
+        // Fixes "webkit blob resource error 1"
+        setTimeout(function() {
+            document.body.removeChild(tempLink);
+            window.URL.revokeObjectURL(blobURL);
+        }, 200)
+    }
 }
+
+convertBtn.addEventListener('click', () => {
+    UrlInput.setAttribute("disabled", "disabled")
+    convertBtn.setAttribute("disabled", "disabled")
+
+    fetch(`./download?url=${UrlInput.value}`, {
+        method:'GET'
+    })
+    .then((res) => {
+        fileDownload(res)
+        UrlInput.removeAttribute("disabled")
+        UrlInput.value = ""
+        convertBtn.removeAttribute("disabled")
+    })
+});
