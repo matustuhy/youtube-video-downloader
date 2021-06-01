fdcf35dc9a9755f9fe4cb96479c7a6a6a92cb837
Matus Tuhy <matustuhy@gmail.com>
2021-06-01 10:41:45 +0200

    Fixed issue with downloading characters with some special names

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
+
 const app = express();
+
+const replacements = [[/'/g, ''], [/\|/g, ''], [/'/g, ''], [/\//g, ''], [/\?/g, ''], [/:/g, ''], [/;/g, ''], [/•/g, '']]
+
 app.use(cors());
 app.use(express.static('public'))
+
 const port = process.env.PORT || 80
+
 app.listen(port, () => {
     console.log(`Server Works !!! At port ${port}`);
 });
+
 app.get('/download', async (req,res) => {
     const url = req.query.url;
     const fileInfo = await ytdl.getInfo(url)
 
-    const fileName = `${fileInfo.videoDetails.title}.mp4`
+    let fileName = `${fileInfo.videoDetails.title}.mp4`
+
+    replacements.forEach(function(replacement) {
+        fileName = fileName.replace(replacement[0], replacement[1]);
+    });
+
+    console.log(fileName)
+
     res.header('Content-Disposition', `attachment; filename="${fileName}"`);
     res.header('Content-Type', "video/mp4");
     res.header('File-Name', fileName);
