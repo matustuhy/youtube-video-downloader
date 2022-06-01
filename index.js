40340ef6de157512f8b2c2b32fa354ddf345b335
Matus Tuhy <matustuhy@gmail.com>
2022-06-01 13:34:40 +0200

    added error catching

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ytdl = require('ytdl-core');
 
 const app = express();
 
-const replacements = [[/'/g, ''], [/\|/g, ''], [/'/g, ''], [/\//g, ''], [/\?/g, ''], [/:/g, ''], [/;/g, ''], [/•/g, ''], [/’/g, '']]
+const replacements = [[/'/g, ''], [/\|/g, ''], [/'/g, ''], [/\//g, ''], [/\?/g, ''], [/:/g, ''], [/;/g, ''], [/•/g, ''], [/’/g, ''], [/([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g, '']]
 
 app.use(cors());
 app.use(express.static('public'))
@@ -15,20 +15,24 @@ app.listen(port, () => {
     console.log(`Server Works !!! At port ${port}`);
 });
 
-app.get('/download', async (req,res) => {
+app.get('/download', async (req, res) => {
     const url = req.query.url;
     const fileInfo = await ytdl.getInfo(url)
 
     let fileName = `${fileInfo.videoDetails.title}.mp4`
 
-    replacements.forEach(function(replacement) {
+    replacements.forEach(function (replacement) {
         fileName = fileName.replace(replacement[0], replacement[1]);
     });
 
-    console.log(fileName)
+    console.log("Downloading", fileName)
 
-    res.header('Content-Disposition', `attachment; filename="${fileName}"`);
-    res.header('Content-Type', "video/mp4");
-    res.header('File-Name', fileName);
-    ytdl(url, { format: 'mp4' }).pipe(res);
+    try {
+        res.header('Content-Disposition', `attachment; filename="${fileName}"`);
+        res.header('Content-Type', "video/mp4");
+        res.header('File-Name', fileName);
+        ytdl(url, {format: 'mp4'}).pipe(res);
+    } catch (e) {
+        console.error(e.message)
+    }
 });
